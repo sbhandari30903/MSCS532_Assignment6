7fae088fcfeb7e4d0adfe0157bb0c6a80100cb83
sbhandari30903 <sbhandari30903@ucumberlands.edu>
2025-02-08 23:18:28 -0500

    implemented quick select and median of medians.

---
+++ SelectionAlgorithms/median_of_medians.py
@@ -0,0 +1,29 @@
+# arr is the array from where kth smallest element is to be selected
+# kth smallest element k starts from 0
+def median_of_medians(arr, k):
+    # returns kth element from sorted arr if arr size is 5 or less 
+    if len(arr) <= 5:
+        return sorted(arr)[k]
+    # arr_size_5 contains sub array of size 5 of the original array
+    arr_size_5 = [arr[i:i+5] for i in range(0, len(arr), 5)]
+    # contains median from sorted size 5 array 
+    medians = [sorted(ar)[len[ar]//2] for ar in arr_size_5]
+
+    # recurssively gets the median of medians
+    pivot = median_of_medians(medians, len(medians)//2)
+
+    # slices the array into left and right based on pivot
+    left = [x for x in arr if x < pivot]
+    # middle contains pivot elements 
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    # if the kth element is on the left recursively get kth element
+    if k < len(left):
+        return median_of_medians(left, k)
+    # if in in middle array simply returns pivot
+    elif k < (len(left) + len(middle)):
+        return pivot
+    # if on right recursively return kth of original array but (kth -left array - middle array of right array)
+    else:
+        return median_of_medians(right, k - len(left) - len(middle))
---
+++ SelectionAlgorithms/randomized_quickselect.py
@@ -0,0 +1,63 @@
+import random
+
+"""
+Partitions the array based on randomly chosen pivot.
+param arr: The array to partition.
+param l: The starting index of the subarray.
+param r: The ending index of the subarray.
+return: The index of the pivot after partitioning.
+"""
+def partation(arr, l, r):
+    # get random pivot index
+    pivot_index = random.randint(l, r)
+    # move pivot element to the end
+    arr[r], arr[pivot_index] = arr[pivot_index], arr[r]
+    pivot = arr[r]
+    i = l - 1
+    for j in range(l, r):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    # moves the pivot to the correct position
+    arr[i+1], arr[r] = arr[r], arr[i+1]
+    return i+1
+
+
+"""
+Finds the k-th smallest element in arr using Randomized Quickselect.
+param arr: The array to search.
+param l: The starting index of the subarray.
+param h: The ending index of the subarray.
+param k: The k-th smallest element to find (1-based index).
+return: The k-th smallest element.
+raises ValueError: If k is out of bounds (k > len(arr) or k <= 0).
+"""
+def randomized_quickselect(arr, l, r, k):
+     # Handle invalid k values
+    if k <= 0 or k > len(arr):
+        raise ValueError(f"k must be between 1 and {len(arr)}. Got k = {k}.")
+    # the left should be smaller than right
+    if l < r:
+        # gets the partion index with partitioned array
+        pivot = partation(arr, l, r)
+
+        # recursively returns the kth smallest element if the k is smaller than pivot
+        if k-1 < pivot:
+            return randomized_quickselect(arr, l, pivot-1, k)
+        # returns the pivot element if the kth samllest is pivot
+        elif k-1 == pivot:
+            return arr[pivot]
+        # recursively returns the kth element from right part if the kth is grater than pivot
+        else:
+            return randomized_quickselect(arr, pivot+1, r, k)
+        
+
+arr = [3, 6, 2, 7, 5, 1, 4, 9, 8]
+k = 4
+try:
+    result = randomized_quickselect(arr,0, len(arr)-1, k)
+    print(f"The {k}-th smallest element is: {result}")
+except ValueError as e:
+    print(e)
+
+
